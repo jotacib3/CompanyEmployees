26c65b6705ab71356d43f55790335094dbb90d75
jotacib3 <jotacib3@gmail.com>
2022-04-04 01:14:25 -0400

    chore(Extensions): create static class ServiceExtensions and add methods ConfigureCors and ConfigureIISIntegration

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection service) =>
+            service.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                 builder.AllowAnyOrigin()
+                 .AllowAnyHeader()
+                 .AllowAnyMethod());
+            });
+
+        public static IServiceCollection ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options => {});
+
+    }
+}
