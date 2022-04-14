a51ecee2e603afcfe1e0db79dc636c300ba8f063
jotacib3 <jotacib3@gmail.com>
2022-04-14 17:31:08 -0400

    chore(Extensions): add ConfigureLoggerService to ServiceExtensions class

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Contracts;
+using LoggerService;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CompanyEmployees.Extensions
@@ -16,6 +18,9 @@ namespace CompanyEmployees.Extensions
 
         public static IServiceCollection ConfigureIISIntegration(this IServiceCollection services) =>
             services.Configure<IISOptions>(options => {});
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddScoped<ILoggerManager, LoggerManager>();
 
     }
 }
